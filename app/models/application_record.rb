241b5075f4ba390095983d6f207191f6b46cea7b
Samet Polat <samet.polat@iwallet.com.tr>
2026-06-11 13:31:28 +0300

    refactor(i18n): source enum labels from locale files via enum_label

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.enum_label(attribute, value)
+    I18n.t(value, scope: [ :activerecord, :attributes, model_name.i18n_key, attribute ], default: value.to_s.titleize)
+  end
 end
