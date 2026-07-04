9b2a82bb36d6ce94ef6757f9974681c92149bfe5
Samet Polat <samet.polat@iwallet.com.tr>
2026-07-04 19:55:02 +0300

    refactor(public): drop trainers, lessons, and gallery actions from clubs

--- app/controllers/public/clubs_controller.rb
+++ app/controllers/public/clubs_controller.rb
@@ -1,15 +1,3 @@
 class Public::ClubsController < Public::BaseController
   def show; end
-
-  def trainers
-    @trainers = Trainer.ordered.with_attached_photo.includes(:disciplines)
-  end
-
-  def lessons
-    @schedule_days  = Lesson.schedule_days
-    @schedule_hours = Lesson.schedule_hours
-    @schedule_kinds = Lesson.schedule_kinds
-  end
-
-  def gallery; end
 end
