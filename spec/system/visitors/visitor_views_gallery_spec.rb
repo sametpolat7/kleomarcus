c4359ac40d071bf2eebdc795f18059d2c628e952
Samet Polat <samet.polat@iwallet.com.tr>
2026-07-04 19:55:40 +0300

    test(system): visit gallery and trainers pages via standalone routes

--- spec/system/visitors/visitor_views_gallery_spec.rb
+++ spec/system/visitors/visitor_views_gallery_spec.rb
@@ -2,19 +2,19 @@ require "rails_helper"
 
 RSpec.describe "Gallery Page", type: :system do
   it "loads successfully" do
-    visit gallery_club_path
+    visit gallery_path
     expect(page).to have_http_status(:success)
   end
 
   describe "gallery lightbox", :js do
     it "opens lightbox when gallery item is clicked" do
-      visit gallery_club_path
+      visit gallery_path
       first("[data-gallery-target='item']").click
       expect(page).to have_selector("dialog.modal[open]", visible: true)
     end
 
     it "navigates to next image when next button is clicked" do
-      visit gallery_club_path
+      visit gallery_path
       first("[data-gallery-target='item']").click
       within("dialog.modal[open]") do
         find("button[aria-label='Sonraki']").click
@@ -23,7 +23,7 @@ RSpec.describe "Gallery Page", type: :system do
     end
 
     it "closes lightbox when close button is clicked" do
-      visit gallery_club_path
+      visit gallery_path
       first("[data-gallery-target='item']").click
       within("dialog.modal[open]") do
         find("button[aria-label='Kapat']").click
--- spec/system/visitors/visitor_views_trainers_spec.rb
+++ spec/system/visitors/visitor_views_trainers_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Trainers Page", type: :system do
   it "loads successfully" do
-    visit trainers_club_path
+    visit trainers_path
     expect(page).to have_http_status(:success)
   end
 
@@ -12,13 +12,13 @@ RSpec.describe "Trainers Page", type: :system do
     end
 
     it "opens modal when trainer card is clicked" do
-      visit trainers_club_path
+      visit trainers_path
       first(".cursor-pointer[data-action*='modal#open']").click
       expect(page).to have_selector("dialog.modal[open]", visible: true)
     end
 
     it "closes modal when close button is clicked" do
-      visit trainers_club_path
+      visit trainers_path
       first(".cursor-pointer[data-action*='modal#open']").click
       within("dialog.modal[open]") do
         find("button.btn-circle").click
