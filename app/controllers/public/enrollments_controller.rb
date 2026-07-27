15bcab260b5d6d6d902855d5870320f3bc9fa62f
Samet Polat <samet.polat@iwallet.com.tr>
2026-07-27 17:06:52 +0300

    refactor(public): drop discipline scoping and mailer from enrollments

--- app/controllers/public/enrollments_controller.rb
+++ app/controllers/public/enrollments_controller.rb
@@ -1,33 +1,26 @@
 class Public::EnrollmentsController < Public::BaseController
-  before_action :set_discipline
-
   def new
-    @enrollment = @discipline.enrollments.build
+    @enrollment = Enrollment.new
   end
 
   def create
-    @enrollment = @discipline.enrollments.build(enrollment_params)
+    @enrollment = Enrollment.new(enrollment_params)
 
     if @enrollment.save
-      EnrollmentMailer.new_application(@enrollment).deliver_later
       session[:enrollment_completed] = true
-      redirect_to enrollment_thanks_path(@discipline)
+      redirect_to thanks_enrollment_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def thanks
-    redirect_to new_enrollment_path(@discipline) unless session.delete(:enrollment_completed)
+    redirect_to new_enrollment_path unless session.delete(:enrollment_completed)
   end
 
   private
 
-  def set_discipline
-    @discipline = Discipline.find_by!(slug: params[:discipline])
-  end
-
   def enrollment_params
-    params.expect(enrollment: [ :full_name, :phone, :email, :age, :level, :message, :info_consent, :kvkk_consent ])
+    params.expect(enrollment: [ :full_name, :phone, :email, :age, :message, :info_consent, :kvkk_consent ])
   end
 end
