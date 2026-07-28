d1b15b4723d22caed5e16916131ea5d8f8b8b3f6
Samet Polat <samet.polat@iwallet.com.tr>
2026-07-28 16:23:01 +0300

    fix(admin): clamp out-of-range pagination pages instead of raising

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,6 +3,13 @@ class Admin::BaseController < ApplicationController
   include Pagy::Backend
 
   before_action :require_panel_access
-
   layout "admin"
+
+  private
+
+  def pagy_get_page(vars, force_integer: true)
+    page = super
+
+    force_integer ? [ page, 1 ].max : page
+  end
 end
--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,3 +1,5 @@
 require "pagy"
+require "pagy/extras/overflow"
 
 Pagy::DEFAULT[:limit] = 10
+Pagy::DEFAULT[:overflow] = :last_page
