843aee798c028a4de177c8fb2341e99b252921c6
Samet Polat <samet.polat@iwallet.com.tr>
2026-07-28 19:00:01 +0300

    refactor(models): read enum labels from the activerecord.enums scope

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,6 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   def self.enum_label(attribute, value)
-    I18n.t(value, scope: [ :activerecord, :attributes, model_name.i18n_key, attribute ], default: value.to_s.titleize)
+    I18n.t(value, scope: [ :activerecord, :enums, model_name.i18n_key, attribute ], default: value.to_s.titleize)
   end
 end
