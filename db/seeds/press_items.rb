467bfc257c6ad5a68e5269ddeef8e382354534ef
Samet Polat <samet.polat@iwallet.com.tr>
2026-09-04 00:03:07 +0300

    feat(seeds): load press items from db/seeds/press_items.rb in seeds

---
+++ db/seeds/press_items.rb
@@ -0,0 +1,201 @@
+press_items_data = [
+  {
+    publisher: "Çanakkale Olay",
+    publisher_kind: :local_press,
+    headline: "Kleomarcus'tan Mazlum Orak ve Ceyda Güven Balkan Şampiyonası'nda Ay-Yıldızlı Formayı Taşıyacak",
+    url: "https://www.canakkaleolay.com/haber/kleomarcus-tan-mazlum-orak-ve-ceyda-guven-balkan-sampiyonasi-nda-ay-yildizli-formayi-tasiyacak-113182",
+    archive_url: "https://web.archive.org/web/20251102184308/https://www.canakkaleolay.com/haber/kleomarcus-tan-mazlum-orak-ve-ceyda-guven-balkan-sampiyonasi-nda-ay-yildizli-formayi-tasiyacak-113182",
+    published_on: Date.new(2025, 10, 31),
+    byline: "Hadiye Ayşe İrim",
+    quote: "Kleomarcus Spor Akademisi kurucusu ve Milli Takım Antrenörü Mazlum Orak ve kulüp sporcusu Ceyda Güven, 13-17 Kasım 2025 tarihleri arasında Bulgaristan'ın Burgaz şehrinde yapılacak Balkan Wushu Şampiyonası'na katılacak."
+  },
+  {
+    publisher: "Çanakkale Olay",
+    publisher_kind: :local_press,
+    headline: "Kleomarcus'tan Ceyda Güven, Balkan Şampiyonasından gümüş madalya ile döndü",
+    url: "https://www.canakkaleolay.com/haber/kleomarcus-tan-ceyda-guven-balkan-sampiyonasindan-gumus-madalya-ile-dondu-114206",
+    archive_url: "https://web.archive.org/web/20251207164751/https://www.canakkaleolay.com/haber/kleomarcus-tan-ceyda-guven-balkan-sampiyonasindan-gumus-madalya-ile-dondu-114206",
+    published_on: Date.new(2025, 11, 16),
+    byline: "Hadiye Ayşe İrim",
+    quote: "70 kilo Kadınlar Sanda Kategorisinde Balkan İkincisi olarak, gümüş madalya kazandı."
+  },
+  {
+    publisher: "Çanakkale Olay",
+    publisher_kind: :local_press,
+    headline: "Genç sporcu Muhammed Cemal Yay, şampiyonluk yolunda!",
+    url: "https://www.canakkaleolay.com/haber/genc-sporcu-muhammed-cemal-yay-sampiyonluk-yolunda-116433",
+    archive_url: "https://web.archive.org/web/20260122223656/https://www.canakkaleolay.com/haber/genc-sporcu-muhammed-cemal-yay-sampiyonluk-yolunda-116433",
+    published_on: Date.new(2025, 12, 22),
+    byline: "Hadiye Ayşe İrim",
+    quote: "2024 yılında Edirne'de düzenlenen 7. Açık Balkan Wushu Şampiyonası'nda gümüş madalya kazanarak adından söz ettirdi."
+  },
+  {
+    publisher: "Çanakkale Olay",
+    publisher_kind: :local_press,
+    headline: "Çanakkale'de ringler kabuk değiştiriyor!",
+    url: "https://www.canakkaleolay.com/haber/canakkale-de-ringler-kabuk-degistiriyor-124262",
+    archive_url: "https://web.archive.org/web/20260903203103/https://www.canakkaleolay.com/haber/canakkale-de-ringler-kabuk-degistiriyor-124262",
+    published_on: Date.new(2026, 7, 22),
+    byline: "Azize Yalçın, Simay Yıldırım",
+    quote: "Kulübümüzün %80'ini kadınlar oluşturuyor. Çocuklarda akran zorbalığı, kadınlarda ise öz savunma ve sağlıklı yaşam isteği talebi zirveye taşıdı."
+  },
+  {
+    publisher: "Çanakkale Kalem",
+    publisher_kind: :local_press,
+    headline: "KickBoks'ta büyük başarı",
+    url: "https://www.canakkalekalem.com/kickboksta-buyuk-basari/12207",
+    archive_url: "https://web.archive.org/web/20260903202655/https://www.canakkalekalem.com/kickboksta-buyuk-basari/12207",
+    published_on: Date.new(2019, 4, 26),
+    quote: "ÇOMÜ Kleomarcus Kulübü sporcuları, Bartın'da düzenlenen Üniversiteler Arası Kick Boks Türkiye Şampiyonası'nda dereceye girdi."
+  },
+  {
+    publisher: "Çanakkale Kalem",
+    publisher_kind: :local_press,
+    headline: "Çanakkale sporcuları 7. Açık Balkan Wushu Şampiyonası'ndan madalya ile döndü",
+    url: "https://www.canakkalekalem.com/canakkale-sporculari-7-acik-balkan-wushu-sampiyonasi-ndan-madalya-ile-dondu/131006",
+    archive_url: "https://web.archive.org/web/20250803015336/https://www.canakkalekalem.com/canakkale-sporculari-7-acik-balkan-wushu-sampiyonasi-ndan-madalya-ile-dondu/131006",
+    published_on: Date.new(2024, 7, 24),
+    quote: "23 ülkeden 1100 sporcunun katıldığı şampiyonaya Milli Takım Antrenörü Mazlum Orak eşliğinde katılan sporcular, Light Sanda kategorisinde ikincilik elde etti."
+  },
+  {
+    publisher: "Çanakkale Kalem",
+    publisher_kind: :official_statement,
+    headline: "2025 Wushu Türkiye Şampiyonasında büyük başarı elde etti!",
+    url: "https://www.canakkalekalem.com/2025-wushu-turkiye-sampiyonasinda-buyuk-basari-elde-etti/139832",
+    archive_url: "https://web.archive.org/web/20260903203230/https://www.canakkalekalem.com/2025-wushu-turkiye-sampiyonasinda-buyuk-basari-elde-etti/139832",
+    published_on: Date.new(2025, 2, 18),
+    quote: "Çanakkale Gençlik ve Spor İl Müdürlüğü, Kleo Marcus Spor Akademisi'ni tebrik ederek sporcuların derecelerini duyurdu."
+  },
+  {
+    publisher: "Çanakkale Kalem",
+    publisher_kind: :official_statement,
+    headline: "Çanakkale'den Balkanlara Milli Güç! Wushu'da Ceyda Güven'den Türkiye'ye GÜMÜŞ MADALYA!",
+    url: "https://www.canakkalekalem.com/canakkaleden-balkanlara-milli-guc-wushuda-ceyda-guvenden-turkiyeye-gumus-madalya/157286",
+    archive_url: "https://web.archive.org/web/20251205173431/https://www.canakkalekalem.com/canakkaleden-balkanlara-milli-guc-wushuda-ceyda-guvenden-turkiyeye-gumus-madalya/157286",
+    published_on: Date.new(2025, 11, 19),
+    byline: "Mustafa Aydın Gül",
+    quote: "Çanakkale'yi temsil eden milli sporcu Ceyda Güven, 70 kilo Kadınlar Sanda kategorisinde gösterdiği üstün performansla ülkemize gümüş madalya kazandırdı."
+  },
+  {
+    publisher: "Burası Çanakkale",
+    publisher_kind: :local_press,
+    headline: "Ceyda Güven'e Open Balkan Wushu Games'de Gümüş Madalya",
+    url: "https://www.burasicanakkale.com.tr/haber/71334-ceyda-guvene-open-balkan-wushu-gamesde-gumus-madalya",
+    archive_url: "https://web.archive.org/web/20251213073421/https://www.burasicanakkale.com.tr/haber/71334-ceyda-guvene-open-balkan-wushu-gamesde-gumus-madalya",
+    published_on: Date.new(2025, 11, 19),
+    byline: "Bünyamin Nami Tonka"
+  },
+  {
+    publisher: "Çanakkale Olay",
+    publisher_kind: :local_press,
+    headline: "Çanakkale'den Türkiye Wushu Kung Fu Şampiyonası'nda büyük başarı",
+    url: "https://www.canakkaleolay.com/haber/canakkale-den-turkiye-wushu-kung-fu-sampiyonasi-nda-buyuk-basari-83243",
+    archive_url: "https://web.archive.org/web/20250326111712/https://canakkaleolay.com/haber/canakkale-den-turkiye-wushu-kung-fu-sampiyonasi-nda-buyuk-basari-83243",
+    published_on: Date.new(2025, 2, 16),
+    byline: "Sevi Gözay Uğurlu",
+    quote: "Şampiyonaya Çanakkale'den Kleomarcus Spor Akademisi 5 sporcu ile katıldı. Şampiyonada; sporculardan Azra Kocakuşak Türkiye şampiyonu olurken, Muhammed Cemal Yay ve Semanur Karaman Türkiye üçüncüsü oldu."
+  },
+  {
+    publisher: "Çanakkale Olay",
+    publisher_kind: :local_press,
+    headline: "Çanakkale'yi Balkan Şampiyonası'nda temsil edecekler",
+    url: "https://www.canakkaleolay.com/haber/canakkale-yi-balkan-sampiyonasi-nda-temsil-edecekler-67277",
+    archive_url: "https://web.archive.org/web/20260903202102/https://www.canakkaleolay.com/haber/canakkale-yi-balkan-sampiyonasi-nda-temsil-edecekler-67277",
+    published_on: Date.new(2024, 7, 19),
+    quote: "Kleomarcus Spor Kulübü Antrenörü Mazlum Orak tarafından çalıştırılan Süheyla Altın, Süreyya Altın ve Muhammet Cemal Yay'dan oluşan Çanakkale ekibi, ilimizi Edirne'de düzenlenecek olan 7. Açık Balkan Wushu Kung Fu Şampiyonası'nda temsil edecek."
+  },
+  {
+    publisher: "Çanakkale Gündem",
+    publisher_kind: :local_press,
+    headline: "Wushu'ya Çanakkale Dokunuşu",
+    url: "https://canakkalegundem.net/haber/26091697/wushuya-canakkale-dokunusu",
+    archive_url: "https://web.archive.org/web/20260903202208/https://canakkalegundem.net/haber/26091697/wushuya-canakkale-dokunusu",
+    published_on: Date.new(2024, 2, 29),
+    byline: "Metin Pala",
+    quote: "Çanakkale Kleomarcus Spor Klubü, 16-28 Şubat tarihleri arasında Yalova'da yapılan Türkiye Wushu Şampiyonası'ndan 2 gümüş, 2 bronz madalya ile döndü."
+  },
+  {
+    publisher: "Çanakkale Gençlik ve Spor İl Müdürlüğü",
+    publisher_kind: :official_statement,
+    headline: "2025 Wushu Türkiye Şampiyonasında Kleo Marcus Spor Akademisi Sporcularından Büyük Başarı",
+    url: "https://www.instagram.com/p/DGLI5T1oHzH/",
+    published_on: Date.new(2025, 2, 17),
+    quote: "Büyükler Kategorisi: Ceyda Güven - Türkiye Şampiyonu (1.), Azra Kocakuşak - Türkiye Şampiyonu (1.), Muhammed Cemal Yay - Takım Sporlarında Türkiye Şampiyonu, Muhammed Cemal Yay - Bireyselde Türkiye 3.sü. Gençler Kategorisi: Semanur Karaman - Türkiye 3.sü. Sporcu ve antrenörlerini tebrik eder, başarılarının devamını dileriz!"
+  },
+  {
+    publisher: "Çanakkale Onsekiz Mart Üniversitesi",
+    publisher_kind: :official_statement,
+    headline: "BESYO Sporcuları'nın Şampiyona Başarısı",
+    url: "https://www.comu.edu.tr/duyuru-14762.html",
+    archive_url: "https://web.archive.org/web/20260903202810/https://www.comu.edu.tr/duyuru-14762.html",
+    published_on: Date.new(2016, 4, 28),
+    byline: "Duygu Karademir Aras",
+    quote: "Marmaris'te yapılan Kickboks Üniversitelerarası Türkiye Şampiyonası sonuçlarına göre; Low Kick Müsabakalarında, Mazlum Orak ve Rıfat Kaya Türkiye 3.sü oldu."
+  },
+  {
+    publisher: "Burası Çanakkale",
+    publisher_kind: :local_press,
+    headline: "ÇOMÜ takımı Türkiye birincisi oldu",
+    url: "https://www.burasicanakkale.com.tr/haber/12369-comu-takimi-turkiye-birincisi-oldu",
+    archive_url: "https://web.archive.org/web/20260515173916/https://www.burasicanakkale.com.tr/haber/12369-comu-takimi-turkiye-birincisi-oldu",
+    published_on: Date.new(2019, 4, 29),
+    byline: "Bünyamin Nami Tonka",
+    quote: "17-20 Nisan tarihleri arasında Bartın ilinde gerçekleştirilen Üniversitelerarası Wushu Kungfu Türkiye Şampiyonasına Antrenör Mazlum Orak nezaretinde katılan Çanakkale Onsekiz Mart Üniversitesi (ÇOMÜ) Wushu Kungfu Takımı, 12 sporcu ile katıldığı turnuvadan 12 madalya ile döndü."
+  },
+  {
+    publisher: "SES15",
+    publisher_kind: :local_press,
+    headline: "İkiz Kız Kardeşler, Milli Takım Formasına Kavuşabilmek İçin Ter Döküyor",
+    url: "https://www.ses15.com.tr/genel/ikiz-kiz-kardesler-milli-takim-formasina-kavusabilmek-icin-ter-dokuyor-116659",
+    archive_url: "https://web.archive.org/web/20250322062649/https://www.ses15.com.tr/genel/ikiz-kiz-kardesler-milli-takim-formasina-kavusabilmek-icin-ter-dokuyor-116659",
+    published_on: Date.new(2025, 1, 23),
+    quote: "Eğitimlerinin yanı sıra bir spor branşıyla ilgilenmeye karar veren ikiz kardeşlerin yolu, yaptıkları araştırmalar neticesinde Wushu Milli Takımı antrenörü Mazlum Orak ile kesişti."
+  },
+  {
+    publisher: "Kalenin Sesi",
+    publisher_kind: :local_press,
+    headline: "Spor Toplumun Refahı İçin Önemli",
+    url: "https://www.kaleninsesi.com/haber/14450353/spor-toplumun-refahi-icin-onemli-11122020",
+    archive_url: "https://web.archive.org/web/20230709125804/https://www.kaleninsesi.com/haber/14450353/spor-toplumun-refahi-icin-onemli-11122020",
+    published_on: Date.new(2020, 12, 11),
+    quote: "Kleomarcus kulübünün milli sporcusu aynı zamanda başkanı olan Mazlum Orak pandemi sürecinde spor'dan uzak kalınmaması gerektiğini ifade etti."
+  },
+  {
+    publisher: "Kalenin Sesi",
+    publisher_kind: :local_press,
+    headline: "Ailelerin Mücadele Sporlarına Bakışları Değişmeli",
+    url: "https://www.kaleninsesi.com/haber/14456416/ailelerin-mucadele-sporlarina-bakislari-degismeli-17072020",
+    published_on: Date.new(2020, 7, 17),
+    quote: "Kleomarcus kulübünün milli sporcusu aynı zamanda mücadele sporu antrenörü olan Mazlum Orak yaptığı açıklamalarda ailelerin mücadele sporlarına olan bakış açılarının değişmesi gerektiğini ifade etti."
+  },
+  {
+    publisher: "Kalenin Sesi",
+    publisher_kind: :local_press,
+    headline: "Milli Formayla Başarılara İmza Atacağım",
+    url: "https://www.kaleninsesi.com/haber/14434724/milli-formayla-basarilara-imza-atacagim-05052020",
+    published_on: Date.new(2020, 5, 5),
+    quote: "Kleomarcus Spor Kulübünün Milli sporcusu Nadire Dilek ile spor hayatını konuştuk."
+  },
+  {
+    publisher: "Kalenin Sesi",
+    publisher_kind: :local_press,
+    headline: "WUSHU SANDA'DA TÜRKİYE DERECELERİ GELDİ",
+    url: "https://www.kaleninsesi.com/haber/14441499/wushu-sandada-turkiye-dereceleri-geldi-12022020",
+    published_on: Date.new(2020, 2, 12)
+  },
+  {
+    publisher: "Kalenin Sesi",
+    publisher_kind: :local_press,
+    headline: "AYYILDIZI GURURLA TAŞIYORUM",
+    url: "https://www.kaleninsesi.com/haber/14455241/ayyildizi-gururla-tasiyorum-15122019",
+    published_on: Date.new(2019, 12, 15),
+    quote: "2005 doğumlu olan Nadire Dilek bir yıl önce Wushu Kung-fu sporuna Kleomarcus Spor Kulübünde antrenör Mazlum Orak'ın yanında başlarken disiplinli ve sıkı çalışmasının karşılığını katıldığı turnuvalarda elde ettiği başarılarla aldı."
+  }
+]
+
+press_items_data.each do |attrs|
+  press_item = PressItem.find_or_initialize_by(url: attrs[:url])
+  press_item.assign_attributes(attrs.except(:url))
+  press_item.published = true if press_item.new_record?
+  press_item.save!
+end
